717e82854473c4cd39c0fd29cf88e772da654281
DESKTOP-4IJBB1U\Z3 <kanemarum8@gmail.com>
2023-11-27 12:55:49 +0900

    画面遷移のスクリプトかきかき

---
+++ AIGameJam/Assets/Scripts/UI/OptionOnOff.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionOnOff : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
